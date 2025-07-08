1a6771410d8f859a12a5c972670e6937578a0b78
Your Name <brady.m.adams@gmail.com>
2025-07-08 06:26:10 -0600

    line numbers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,3 +2,7 @@
 -- 'l' to move left and ';' to move right
 vim.keymap.set('n', 'l', 'h', { noremap = true })
 vim.keymap.set('n', ';', 'l', { noremap = true })
+
+-- Enable line numbers with relative numbering
+vim.opt.number = true      -- Show current line number
+vim.opt.relativenumber = true  -- Show relative line numbers
