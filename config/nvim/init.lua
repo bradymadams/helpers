423460a3aff6a2f983fc7eb032bb6835d63aece6
Brady Adams <brady.m.adams@gmail.com>
2025-07-09 09:15:08 -0600

    some more minor adj

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -22,18 +22,5 @@ require("config.statusline")
 require("config.telescope")
 require("config.treesitter")
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  callback = function()
-    -- check if treesitter has parser
-    if require("nvim-treesitter.parsers").has_parser() then
-      -- use treesitter folding
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    else
-      -- use alternative foldmethod
-      vim.opt.foldmethod = "syntax"
-    end
-  end,
-})
 
 vim.cmd("colorscheme dracula")
--- config/nvim/lua/core.lua
+++ config/nvim/lua/core.lua
@@ -11,6 +11,7 @@ vim.opt.wrap = false
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 0
+vim.opt.mouse = ""
 
 -- Remap navigation keys
 -- 'l' to move left and ';' to move right
@@ -25,6 +26,26 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+-- Folding is broken with this setup. It doesn't really work and some unrelated operations,
+-- like deleting text in visual mode, will activate a bunch of folds. Needs some attention.
+--[[
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function()
+    -- check if treesitter has parser
+    if require("nvim-treesitter.parsers").has_parser() then
+      -- use treesitter folding
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    else
+      -- use alternative foldmethod
+      vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
+]]
+
+vim.opt.foldmethod = "syntax"
 
 -- Autocmd: reload file if changed outside
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
