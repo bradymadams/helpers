ae90452b3daf10d729e99adfb303f47ca932a859
Brady Adams <brady.m.adams@gmail.com>
2025-07-25 12:25:26 -0600

    init of an install common tools script

---
+++ config/install-common-tools.sh
@@ -0,0 +1,17 @@
+# Tools are in alphabetical order
+apt install \
+    clangd \
+    clang-format \
+    cmake \
+    g++ \
+    git \
+    fd-find \
+    jq \
+    make \
+    ripgrep
+
+# Install uv
+
+# Install nvm
+
+# npm i -g pyright typescript typescript-language-server
