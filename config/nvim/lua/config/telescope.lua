9aa97d74bb7b838a5ec6ec8a0c051df7ee88c535
Brady Adams <brady.m.adams@gmail.com>
2025-08-11 10:38:50 -0600

    fix/improve telescope find_files usage

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -10,7 +10,7 @@ require("telescope").setup({
     sorting_strategy = "ascending",
     layout_strategy = "horizontal",
     -- optional extra filters
-    file_ignore_patterns = { "node_modules/", "%.git/", "dist/", "build/", ".venv/", "3rd/" },
+    file_ignore_patterns = { "node_modules/", "%.git/", "dist/", "build/", ".venv/" },
     vimgrep_arguments = {
       "rg",
       "--color=never",
@@ -23,7 +23,6 @@ require("telescope").setup({
       "--glob",
       "!.git/",
     },
-    find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--hidden", "--exclude", ".git" },
   },
 })
 
@@ -35,11 +34,32 @@ function workspace_symbols()
   builtin.lsp_dynamic_workspace_symbols({ symbol_width = 60 })
 end
 
+function find_files()
+  builtin.find_files({
+    no_ignore = true,
+    find_command = {
+      "fdfind",
+      "--type",
+      "f",
+      "--strip-cwd-prefix",
+      "--unrestricted",
+      "--exclude",
+      "node_modules/",
+      "--exclude",
+      ".git/",
+      "--exclude",
+      ".venv/",
+      "--exclude",
+      "*.pyc",
+    },
+  })
+end
+
 -- Load Telescope built-in pickers with keymaps
 local keymap = vim.keymap.set
 
 keymap("n", "<leader>ff", builtin.git_files, { desc = "Find files (git only)" })
-keymap("n", "<leader>fF", builtin.find_files, { desc = "Find files" })
+keymap("n", "<leader>fF", find_files, { desc = "Find files" })
 keymap("n", "<leader>fg", builtin.live_grep, { desc = "Grep files" })
 keymap("n", "<leader>fb", builtin.buffers, { desc = "List open buffers" })
 keymap("n", "<leader>fd", builtin.diagnostics, { desc = "Show diagnostics" })
