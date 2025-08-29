2cf970989c2de41895803945b8016519378aeb87
Your Name <brady.m.adams@gmail.com>
2025-08-28 21:36:13 -0600

    add initial i3 config

--- config/setup.sh
+++ config/setup.sh
@@ -3,11 +3,15 @@
 SCRIPT=$(realpath ${0})
 CFGDIR=$(dirname ${SCRIPT})
 TGTDIR=${1:-${HOME}}
+XDGCFG=${XDG_CONFIG_HOME:-S{HOME}/.config}
 
 ln -sf ${CFGDIR}/.vimrc.HOME ${TGTDIR}/.vimrc
 ln -sf ${CFGDIR}/.tmux.conf ${TGTDIR}/.tmux.conf
 ln -sf ${CFGDIR}/.inputrc ${TGTDIR}/.inputrc
-ln -sf ${CFGDIR}/nvim ${XDG_CONFIG_HOME:-S{HOME}/.config}
+ln -sf ${CFGDIR}/nvim ${XDGCFG}
+
+mkdir -p ${XDGCFG}/i3
+ln -sf ${CFGDIR}/.i3config ${XDGCFG}/i3/config
 
 git config --global --add alias.lg "log --oneline"
 git config --global --add core.editor vim
