762a8e1c7f63f31afc16bcf0c6ae53a3b2c10a1e
Your Name <brady.m.adams@gmail.com>
2025-09-15 20:49:27 -0600

    goofin'

--- kb/keyboards/zsa/moonlander/keymaps/brady/keymap.c
+++ kb/keyboards/zsa/moonlander/keymaps/brady/keymap.c
@@ -16,14 +16,12 @@ enum custom_keycodes {
   ST_MACRO_3,
 };
 
-
-
 enum tap_dance_codes {
   DANCE_0,
   DANCE_1,
 };
 
-
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     OSL(1),         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_COMMA,                                       ST_MACRO_0,     KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
@@ -50,69 +48,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_WWW_BACK
   ),
 };
+// clang-format on
 
-
-const uint16_t PROGMEM combo0[] = { KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo1[] = { KC_P, KC_ENTER, COMBO_END};
+const uint16_t PROGMEM combo0[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo1[] = {KC_P, KC_ENTER, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo0, KC_ESCAPE),
     COMBO(combo1, ST_MACRO_3),
 };
 
-
-
 extern rgb_config_t rgb_matrix_config;
 
 RGB hsv_to_rgb_with_value(HSV hsv) {
-  RGB rgb = hsv_to_rgb( hsv );
+  RGB rgb = hsv_to_rgb(hsv);
   float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-  return (RGB){ f * rgb.r, f * rgb.g, f * rgb.b };
+  return (RGB){f * rgb.r, f * rgb.g, f * rgb.b};
 }
 
-void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
-}
+void keyboard_post_init_user(void) { rgb_matrix_enable(); }
 
+// clang-format off
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [1] = { {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {74,255,206}, {41,255,255}, {41,255,255}, {41,255,255}, {74,255,206}, {74,255,206}, {152,255,255}, {152,255,255}, {152,255,255}, {74,255,206}, {74,255,206}, {152,255,255}, {152,255,255}, {152,255,255}, {74,255,206}, {74,255,206}, {41,255,255}, {41,255,255}, {41,255,255}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206}, {41,255,255}, {41,255,255}, {41,255,255}, {41,255,255}, {74,255,206}, {152,255,255}, {152,255,255}, {152,255,255}, {41,255,255}, {74,255,206}, {152,255,255}, {152,255,255}, {152,255,255}, {152,255,255}, {74,255,206}, {152,255,255}, {152,255,255}, {152,255,255}, {152,255,255}, {74,255,206}, {131,255,255}, {131,255,255}, {131,255,255}, {0,245,245}, {74,255,206}, {0,245,245}, {74,255,206}, {74,255,206}, {74,255,206}, {74,255,206} },
 
     [2] = { {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {74,255,206}, {74,255,206}, {219,255,255}, {219,255,255}, {219,255,255}, {74,255,206}, {74,255,206}, {219,255,255}, {219,255,255}, {219,255,255}, {74,255,206}, {74,255,206}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {74,255,206}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {74,255,206}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {74,255,206}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255}, {219,255,255} },
 
 };
+// clang-format on
 
 void set_layer_color(int layer) {
   for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
     HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
+        .h = pgm_read_byte(&ledmap[layer][i][0]),
+        .s = pgm_read_byte(&ledmap[layer][i][1]),
+        .v = pgm_read_byte(&ledmap[layer][i][2]),
     };
     if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, 0, 0, 0 );
+      rgb_matrix_set_color(i, 0, 0, 0);
     } else {
-        RGB rgb = hsv_to_rgb_with_value(hsv);
-        rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+      RGB rgb = hsv_to_rgb_with_value(hsv);
+      rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
     }
   }
 }
 
 bool rgb_matrix_indicators_user(void) {
   if (rawhid_state.rgb_control) {
-      return false;
+    return false;
   }
-  if (!keyboard_config.disable_layer_led) { 
+  if (!keyboard_config.disable_layer_led) {
     switch (biton32(layer_state)) {
-      case 1:
-        set_layer_color(1);
-        break;
-      case 2:
-        set_layer_color(2);
-        break;
-     default:
-        if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
-          rgb_matrix_set_color_all(0, 0, 0);
-        }
+    case 1:
+      set_layer_color(1);
+      break;
+    case 2:
+      set_layer_color(2);
+      break;
+    default:
+      if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
+        rgb_matrix_set_color_all(0, 0, 0);
+      }
     }
   } else {
     if (rgb_matrix_get_flags() == LED_FLAG_NONE) {
@@ -123,20 +119,18 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
-
-
 typedef struct {
-    bool is_press_action;
-    uint8_t step;
+  bool is_press_action;
+  uint8_t step;
 } tap;
 
 enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
+  SINGLE_TAP = 1,
+  SINGLE_HOLD,
+  DOUBLE_TAP,
+  DOUBLE_HOLD,
+  DOUBLE_SINGLE_TAP,
+  MORE_TAPS
 };
 
 static tap dance_state[2];
@@ -144,77 +138,92 @@ static tap dance_state[2];
 uint8_t dance_step(tap_dance_state_t *state);
 
 uint8_t dance_step(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
-    } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    return MORE_TAPS;
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)
+      return SINGLE_TAP;
+    else
+      return SINGLE_HOLD;
+  } else if (state->count == 2) {
+    if (state->interrupted)
+      return DOUBLE_SINGLE_TAP;
+    else if (state->pressed)
+      return DOUBLE_HOLD;
+    else
+      return DOUBLE_TAP;
+  }
+  return MORE_TAPS;
 }
-
 
 void dance_0_finished(tap_dance_state_t *state, void *user_data);
 void dance_0_reset(tap_dance_state_t *state, void *user_data);
 
 void dance_0_finished(tap_dance_state_t *state, void *user_data) {
-    dance_state[0].step = dance_step(state);
-    switch (dance_state[0].step) {
-        case SINGLE_HOLD: layer_on(1); break;
-        case DOUBLE_HOLD: layer_on(2); break;
-    }
+  dance_state[0].step = dance_step(state);
+  switch (dance_state[0].step) {
+  case SINGLE_HOLD:
+    layer_on(1);
+    break;
+  case DOUBLE_HOLD:
+    layer_on(2);
+    break;
+  }
 }
 
 void dance_0_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    switch (dance_state[0].step) {
-        case SINGLE_HOLD:
-          layer_off(1);
-        break;
-              case DOUBLE_HOLD: 
-                layer_off(2);
-                break;
-    }
-    dance_state[0].step = 0;
+  wait_ms(10);
+  switch (dance_state[0].step) {
+  case SINGLE_HOLD:
+    layer_off(1);
+    break;
+  case DOUBLE_HOLD:
+    layer_off(2);
+    break;
+  }
+  dance_state[0].step = 0;
 }
 void dance_1_finished(tap_dance_state_t *state, void *user_data);
 void dance_1_reset(tap_dance_state_t *state, void *user_data);
 
 void dance_1_finished(tap_dance_state_t *state, void *user_data) {
-    dance_state[1].step = dance_step(state);
-    switch (dance_state[1].step) {
-        case SINGLE_HOLD: layer_on(1); break;
-        case DOUBLE_HOLD: layer_on(2); break;
-    }
+  dance_state[1].step = dance_step(state);
+  switch (dance_state[1].step) {
+  case SINGLE_HOLD:
+    layer_on(1);
+    break;
+  case DOUBLE_HOLD:
+    layer_on(2);
+    break;
+  }
 }
 
 void dance_1_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    switch (dance_state[1].step) {
-        case SINGLE_HOLD:
-          layer_off(1);
-        break;
-              case DOUBLE_HOLD: 
-                layer_off(2);
-                break;
-    }
-    dance_state[1].step = 0;
+  wait_ms(10);
+  switch (dance_state[1].step) {
+  case SINGLE_HOLD:
+    layer_off(1);
+    break;
+  case DOUBLE_HOLD:
+    layer_off(2);
+    break;
+  }
+  dance_state[1].step = 0;
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_0_finished, dance_0_reset),
-        [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset),
+    [DANCE_0] =
+        ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_0_finished, dance_0_reset),
+    [DANCE_1] =
+        ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-  case QK_MODS ... QK_MODS_MAX: 
-    // Mouse keys with modifiers work inconsistently across operating systems, this makes sure that modifiers are always
-    // applied to the mouse key that was pressed.
+  case QK_MODS ... QK_MODS_MAX:
+    // Mouse keys with modifiers work inconsistently across operating systems,
+    // this makes sure that modifiers are always applied to the mouse key that
+    // was pressed.
     if (IS_MOUSE_KEYCODE(QK_MODS_GET_BASIC_KEYCODE(keycode))) {
-    if (record->event.pressed) {
+      if (record->event.pressed) {
         add_mods(QK_MODS_GET_MODS(keycode));
         send_keyboard_report();
         wait_ms(2);
@@ -226,63 +235,69 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
     }
     break;
-    case ST_MACRO_0:
+  case ST_MACRO_0:
     if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_ESCAPE)SS_DELAY(100)  SS_LSFT(SS_TAP(X_SCLN))SS_DELAY(100)  SS_TAP(X_W)  SS_DELAY(100) SS_TAP(X_ENTER));
+      SEND_STRING(SS_TAP(X_ESCAPE) SS_DELAY(100) SS_LSFT(SS_TAP(X_SCLN))
+                      SS_DELAY(100) SS_TAP(X_W) SS_DELAY(100) SS_TAP(X_ENTER));
     }
     break;
-    case ST_MACRO_1:
+  case ST_MACRO_1:
     if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_ESCAPE)SS_DELAY(100)  SS_LSFT(SS_TAP(X_SCLN))SS_DELAY(100)  SS_TAP(X_W)SS_DELAY(100)  SS_TAP(X_A)  SS_DELAY(100) SS_TAP(X_ENTER));
+      SEND_STRING(SS_TAP(X_ESCAPE) SS_DELAY(100) SS_LSFT(SS_TAP(X_SCLN))
+                      SS_DELAY(100) SS_TAP(X_W) SS_DELAY(100) SS_TAP(X_A)
+                          SS_DELAY(100) SS_TAP(X_ENTER));
     }
     break;
-    case ST_MACRO_2:
+  case ST_MACRO_2:
     if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_DOT)SS_DELAY(50)  SS_TAP(X_DOT)SS_DELAY(50)  SS_TAP(X_SLASH));
+      SEND_STRING(SS_TAP(X_DOT) SS_DELAY(50) SS_TAP(X_DOT) SS_DELAY(50)
+                      SS_TAP(X_SLASH));
     }
     break;
-    case ST_MACRO_3:
+  case ST_MACRO_3:
     if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_P)SS_DELAY(50)  SS_TAP(X_Y)SS_DELAY(50)  SS_TAP(X_T)SS_DELAY(50)  SS_TAP(X_H)SS_DELAY(50)  SS_TAP(X_O)SS_DELAY(50)  SS_TAP(X_N)SS_DELAY(50)  SS_TAP(X_SPACE));
+      SEND_STRING(SS_TAP(X_P) SS_DELAY(50) SS_TAP(X_Y) SS_DELAY(50) SS_TAP(X_T)
+                      SS_DELAY(50) SS_TAP(X_H) SS_DELAY(50) SS_TAP(X_O)
+                          SS_DELAY(50) SS_TAP(X_N) SS_DELAY(50)
+                              SS_TAP(X_SPACE));
     }
     break;
 
-    case RGB_SLD:
-        if (rawhid_state.rgb_control) {
-            return false;
-        }
-        if (record->event.pressed) {
-            rgblight_mode(1);
-        }
-        return false;
-    case HSV_0_245_245:
-        if (rawhid_state.rgb_control) {
-            return false;
-        }
-        if (record->event.pressed) {
-            rgblight_mode(1);
-            rgblight_sethsv(0,245,245);
-        }
-        return false;
-    case HSV_74_255_206:
-        if (rawhid_state.rgb_control) {
-            return false;
-        }
-        if (record->event.pressed) {
-            rgblight_mode(1);
-            rgblight_sethsv(74,255,206);
-        }
-        return false;
-    case HSV_152_255_255:
-        if (rawhid_state.rgb_control) {
-            return false;
-        }
-        if (record->event.pressed) {
-            rgblight_mode(1);
-            rgblight_sethsv(152,255,255);
-        }
-        return false;
+  case RGB_SLD:
+    if (rawhid_state.rgb_control) {
+      return false;
+    }
+    if (record->event.pressed) {
+      rgblight_mode(1);
+    }
+    return false;
+  case HSV_0_245_245:
+    if (rawhid_state.rgb_control) {
+      return false;
+    }
+    if (record->event.pressed) {
+      rgblight_mode(1);
+      rgblight_sethsv(0, 245, 245);
+    }
+    return false;
+  case HSV_74_255_206:
+    if (rawhid_state.rgb_control) {
+      return false;
+    }
+    if (record->event.pressed) {
+      rgblight_mode(1);
+      rgblight_sethsv(74, 255, 206);
+    }
+    return false;
+  case HSV_152_255_255:
+    if (rawhid_state.rgb_control) {
+      return false;
+    }
+    if (record->event.pressed) {
+      rgblight_mode(1);
+      rgblight_sethsv(152, 255, 255);
+    }
+    return false;
   }
   return true;
 }
-
