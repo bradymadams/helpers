c34dfc865f340f938b97661bf2cd7d1e69a431a7
Your Name <brady.m.adams@gmail.com>
2026-01-04 07:00:23 -0700

    fix missing macro rec messages

--- config/nvim/lua/config/statusline.lua
+++ config/nvim/lua/config/statusline.lua
@@ -14,3 +14,34 @@ require("lualine").setup({
     lualine_z = { "location" },
   },
 })
+
+--
+-- Usage of Lualine doesn't show notifications about macro recordings, so
+-- the below shows those notifications via vim.notify
+--
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    local msg = string.format("Register: %s", vim.fn.reg_recording())
+    _MACRO_RECORDING_STATUS = true
+    vim.notify(msg, vim.log.levels.INFO, {
+      title = "Macro Recording",
+      keep = function()
+        return _MACRO_RECORDING_STATUS
+      end,
+    })
+  end,
+  group = vim.api.nvim_create_augroup("NoiceMacroNotfication", { clear = true }),
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    local msg = string.format("Finished recording: %s", vim.fn.reg_recording())
+    _MACRO_RECORDING_STATUS = false
+    vim.notify(msg, vim.log.levels.INFO, {
+      title = "Macro Recording",
+      timeout = 2000,
+    })
+  end,
+  group = vim.api.nvim_create_augroup("NoiceMacroNotficationDismiss", { clear = true }),
+})
